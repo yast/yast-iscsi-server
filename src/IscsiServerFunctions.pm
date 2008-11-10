cbb4051fd46aac5e132a7ac8bacec6b89406078f
Michal Žugec <mzugec@suse.cz>
2008-11-10 15:11:45 +0000

    fixed iSNS option problem (bnc#442670)
    svn path=/trunk/iscsi-server/; revision=53137

--- src/IscsiServerFunctions.pm
+++ src/IscsiServerFunctions.pm
@@ -309,7 +309,7 @@ sub writeConfig {
     # write 'iSNS' and 'auth' into %new_config
     $config_file{'value'} = $new_config{'iSNS'};
       delete ($new_config{'iSNS'});
-      @{$config_file{'value'}} = (@{$config_file{'value'}}, @{$new_config{'auth'}}) ;
+    if (exists $new_config{'auth'})  { @{$config_file{'value'}} = (@{$config_file{'value'}}, @{$new_config{'auth'}}) }
       delete ($new_config{'auth'});
     #write all targets into %new_config
     foreach my $key (reverse(keys %new_config )){
